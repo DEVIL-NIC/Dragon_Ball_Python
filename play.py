81c85ff58c02d5b0fd0a79c9b8a0a6939492cc24
Thakur05 <42406489+markrayon@users.noreply.github.com>
2020-01-21 14:41:15 +0530

    Main file to play

---
+++ play.py
@@ -0,0 +1,15 @@
+import sys
+from src import main
+
+w = 0
+help = 0
+for opt in sys.argv:
+    if opt == '-window' or opt == '-w':
+        w = 1
+    if opt == '-help' or opt == '-h':
+        print('-window   -w   Open in window mode')
+        help = 1
+
+if not help:
+    game = main.Main(w, resolution = (1280, 768))
+    game.run()
